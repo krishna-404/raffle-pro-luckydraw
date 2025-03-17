0eaaddbe817e041557460bdbadd459a36c9216ef
Balkrishna Agarwal <krishna@teziapp.com>
2025-03-17 12:11:27 +0530

    removed styling from calendar component to use default styling

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,53 +1,24 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import * as React from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
+	className,
+	classNames,
+	showOutsideDays = true,
+	...props
 }: CalendarProps) {
-  return (
-    <DayPicker
-      showOutsideDays={showOutsideDays}
-      className={cn(
-        "rounded-md border bg-white p-3",
-        className
-      )}
-      classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center px-8",
-        caption_label: "text-sm font-medium",
-        nav: "space-x-1 flex items-center",
-        nav_button: "h-7 w-7 bg-transparent p-0 text-gray-500 hover:text-gray-700",
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "flex justify-between w-full",
-        head_cell: "text-gray-500 w-9 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2 justify-between",
-        cell: "text-center text-sm relative p-0 [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md",
-        day: "h-9 w-9 p-0 font-normal",
-        day_range_end: "day-range-end",
-        day_range_start: "day-range-start",
-        day_selected: "bg-blue-600 text-white hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white",
-        day_today: "bg-gray-100",
-        day_outside: "text-gray-400",
-        day_disabled: "text-gray-400",
-        day_range_middle: "aria-selected:bg-gray-100",
-        day_hidden: "invisible",
-        ...classNames,
-      }}
-      {...props}
-    />
-  );
+	return (
+		<DayPicker
+			showOutsideDays={showOutsideDays}
+			className={cn("rounded-md border bg-white p-3", className)}
+			{...props}
+		/>
+	);
 }
 Calendar.displayName = "Calendar";
 
